d65d2ed2a73bd1b244cdcedf025c6f9d9ce99baa
haozi <syntaxright@gmail.com>
2023-03-31 09:49:24 +0800

    feat: 🎸 dev

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,9 @@
 import { repository, version } from '../../package.json'
+import { BAND_MKTS, BING } from './constants'
 import initContextMenu from './context_menus'
-import initRules from './rules'
-import { getURL, openPage, registryListener } from './utils'
+import initDynamicRules from './dynamic_rules'
+import { getURL, getURLSearchParams, openPage, registryListener } from './utils'
 
-initRules()
 initContextMenu()
 
 registryListener({
@@ -53,6 +53,7 @@ registryListener({
 })
 
 chrome.runtime.onInstalled.addListener((details) => {
+  initDynamicRules()
   const repositoryUrl: string = repository.url
   if (details.reason === 'install') {
     openPage(repositoryUrl)
@@ -60,3 +61,37 @@ chrome.runtime.onInstalled.addListener((details) => {
     openPage(`${repositoryUrl}/releases/tag/${version}`)
   }
 })
+
+chrome.webRequest.onBeforeRequest.addListener(
+  () => {
+    chrome.cookies.get(
+      {
+        name: '_EDGE_S',
+        url: BING
+      },
+      (cookie) => {
+        const value = cookie?.value
+        if (!value) return
+
+        const searchObj = getURLSearchParams(value)
+        const mkt = searchObj.get('mkt')?.toLowerCase() ?? ''
+
+        if (!BAND_MKTS.map((m) => m.toLowerCase()).includes(mkt)) return
+        searchObj.delete('mkt')
+        chrome.cookies.set({
+          url: BING,
+          domain: cookie.domain,
+          name: cookie.name,
+          storeId: cookie.storeId,
+          path: cookie.path,
+          httpOnly: cookie.httpOnly,
+          secure: cookie.secure,
+          sameSite: cookie.sameSite,
+          expirationDate: cookie.expirationDate,
+          value: searchObj.toString()
+        })
+      }
+    )
+  },
+  { urls: [BING + '*'], types: ['main_frame'] }
+)
