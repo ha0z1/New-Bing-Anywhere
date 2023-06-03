28bb5a3c73f96e9a896ab3512b6b2866732df69e
耗子 <syntaxright@gmail.com>
2023-06-03 10:18:21 +0800

    feat: 🎸 Update ua to Edge 113 (#50)

--- src/background/dynamic_rules.ts
+++ src/background/dynamic_rules.ts
@@ -4,8 +4,6 @@ import { genUA } from '@@/utils'
 
 const MODIFY_HEADERS_LIST = {
   // 'X-Forwarded-For': '8.8.8.8',
-  // MAC      Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48
-  // Windows  Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48
   'User-Agent': genUA()
 }
 const MODIFY_HEADERS = 'modifyHeaders' as chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS
