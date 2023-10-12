f54e53e8ca3a5ed541eb6680dbe55aeda038715b
haozi <syntaxright@gmail.com>
2023-10-12 16:17:57 +0800

    feat: 🎸 dev1

--- scripts/_build_app.ts
+++ scripts/_build_app.ts
@@ -4,7 +4,6 @@ import { root } from './_config'
 
 const appDir = path.join(root, 'src/app')
 export default ({ mode }: { mode: 'development' | 'production' }) => {
-  console.log(99999999, appDir)
   const script = mode === 'development' ? 'dev' : 'build'
   process.chdir(appDir)
   spawnSync('bun', ['run', script], { stdio: 'inherit' })
